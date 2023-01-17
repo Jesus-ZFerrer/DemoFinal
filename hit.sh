1e241c094c45164917244a1af71cd9923c0e2e43
Jesus-ZFerrer <122578236+Jesus-ZFerrer@users.noreply.github.com>
2023-01-17 13:58:04 -0600

    Update hit.sh

--- hit.sh
+++ hit.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-if [ -f Outputhits.txt ];then
-	rm -rf Outputhits.txt
+file = Outputhits.txt
+
+clear
+if [ -f $file ];then
+        rm -rf Outputhits.txt
 fi
-	
-read -p "Put a word: " word
+
+read -p "Put a word to check: " word
+echo ""
+read -p "Please put the IP from the container VM: " ip
+echo ""
 read -p "How many time do you want to hit the API? " hit
 
 
 x=1
-while [ $x -le $hit ]	
-	do 
-		curl ipaddressfromcontainer:8000/palindrome/$word >> Outputhits.txt
-		echo " " >> Outputhits.txt
-		echo " " >> Outputhits.txt
-		let x+=1 
-	done
+while [ $x -le $hit ]   
+        do
+                curl $ip/palindrome/$word >> Outputhits.txt
+                echo " " >> Outputhits.txt
+                echo " " >> Outputhits.txt
+                let x+=1
+        done
+echo " " 
+cat Outputhits.txt
+sleep 8
+clear
